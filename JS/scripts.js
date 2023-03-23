722f8344e26b90bde97aad4ede0a8f69b506a1d4
Erik Z <molagg92@gmail.com>
2023-03-23 16:38:06 -0700

    fill the code to more or less create a character creater
    co-authored by: Thomas McDowell <tmcdowellddhs@yahoo.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,30 +1,44 @@
-function nameSubmit(e) {
-  window.e.preventDefault();
+function nameSubmit(event) {
+  event.preventDefault();
   const nameInput = document.getElementById("nameInput").value;
+  document.querySelector("span#nameCharacter").innerText = nameInput
+  let paladinFacts = document.getElementById("paladin");
+  paladinFacts.setAttribute("class","hidden");
+  let rogueFacts = document.getElementById("rogue");
+  rogueFacts.setAttribute("class","hidden");
+  let wizardFacts = document.getElementById("wizard");
+  wizardFacts.setAttribute("class","hidden");
+  let druidFacts = document.getElementById("druid");
+  druidFacts.setAttribute("class","hidden");
+  const operator = document.querySelector("input[name = 'operator']:checked").value;
+  let result;
+    if (operator === "Strong") {
+    result = paladinFacts.removeAttribute("class");
+  } else if (operator === "Nimble") {
+    result = rogueFacts.removeAttribute("class");
+  } else if (operator === "Brilliant") {
+    result = wizardFacts.removeAttribute("class");
+  } else if (operator === "Wise") {
+    result = druidFacts.removeAttribute("class");
+  } 
+  }
+
+function bodySelect(event){
+  event.preventDefault();
+  const archeType = document.querySelector("input[name = 'operator']:checked").value;
+  document.querySelector("span#profession").innerText = archeType
   
 }
+//loader
+window.addEventListener("load", function() {
+  const form = document.getElementById("nameHere");
+  form.addEventListener("submit", nameSubmit);
 
-function bodySelect(){
-  const bodyType = document.getElementById("bodyType").value;
+  const span = document.getElementById("attributeForm");
+  span.addEventListener("submit", bodySelect);
+});
 
-}
-
-
-
-window.addEventListener("load", function(){
-  const nameHere = document.getElementById("nameHere");
-  nameHere.addEventListener("submit", nameSubmit);
-  const selectForm = document.getElementById("selectForm");
-  selectForm.addEventListener("submit", bodySelect);
-  const operator = document.querySelector("input[name='operator']:checked").value;
-  let result;
-  if (operator === "stength") {
-    result = ;
-  } else if (operator === "dexterity") {
-    result = ;
-  } else if (operator === "intelligence") {
-    result = ;
-  } else if (operator === "wisdom") {
-    result = ;
-  }
+document.addEventListener("load", function(){
+  document.querySelector("span#nameCharacter").innerText = nameInput;
+  nameInput.removeAttribute("class");
 });
